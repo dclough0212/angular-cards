9ef42146bf94f55635e0a2eade31712f21ce8682
David Delbert Clough <daviddclough@yahoo.com>
2020-03-09 20:12:26 -0500

    Add Cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-cards';
+  posts = [
+    {
+      title: 'Snowy Mountain',
+      imageUrl: '/assets/mountain.jpeg',
+      username: 'mounttainlover',
+      mycontent: 'Here is a picture of a mountain.'
+    },
+    {
+      title: 'Neat Tree',
+      imageUrl: '/assets/tree.jpeg',
+      username: 'naturedude621',
+      mycontent: 'Here is a picture of a neat tree'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: '/assets/biking.jpeg',
+      username: 'biking1222',
+      mycontent: 'I did some biking today'
+    }
+  ]
 }
